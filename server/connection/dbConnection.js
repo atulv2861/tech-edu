bff84199dbc3fb04f03585909c93ac0fdc051e00
atul2861 <atulverma2861@gmail.com>
2024-10-29 20:33:31 +0530

    first commit

--- server/connection/dbConnection.js
+++ server/connection/dbConnection.js
@@ -2,8 +2,8 @@
 const mongoose = require('mongoose')
 const dbConnection = async()=>{
     try{
-        //const URL = process.env.DB_URL
-        const URL='mongodb+srv://Admin:5xwHUzrYEE0w4Q8l@cluster0.qdjik.mongodb.net/?retryWrites=true&w=majority'
+        const URL = process.env.DB_URL
+        
         await mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true })
         console.log("database connected")
 
